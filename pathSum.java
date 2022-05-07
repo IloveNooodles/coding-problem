784b7f0ab04512a9615b4c4d7bc4c910cb78a836
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2022-05-07 20:11:31 +0700

    Create pathSum.java

---
+++ pathSum.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        // base condition
+        if(root == null) return false;
+        
+        //acc condition
+        if(root.left == null && root.right == null && targetSum - root.val == 0) return true;
+        
+        return (hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val));
+    }
+}
