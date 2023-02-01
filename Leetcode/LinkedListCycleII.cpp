7839e9ffc975fd90ca1202d7000f6053a59f4404
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2023-02-01 09:24:53 +0700

    Create LinkedListCycleII.cpp

---
+++ Leetcode/LinkedListCycleII.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *start = head;
+        while(slow != NULL && fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            // if they meet, let the slow travel from meeting point to the start of the loop
+            //
+            if(slow == fast){
+                while(slow != start){
+                    slow = slow->next;
+                    start = start->next;
+                }
+                return start;
+            }
+
+        }
+        return NULL;
+    }   
+};
