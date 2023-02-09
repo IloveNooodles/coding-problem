3225d364bd63168b71df9afe54a7d3811d38580f
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2023-02-10 06:05:22 +0700

    Update backspaceCompare.cpp

--- Leetcode/backspaceCompare.cpp
+++ Leetcode/backspaceCompare.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
+    stack<char> removeBackspace(string t){
+        stack<char> s;
+        int size = t.size();
+        for(int i = 0; i < size; i++){
+            if(t[i] != '#') s.push(t[i]);
+            if(!s.empty() && t[i] == '#') s.pop();
+        }
+        return s;
+    }
+
     bool backspaceCompare(string s, string t) {
-        stack<char> s1;
-        stack<char> s2;
-        
-        for(char &c: s){
-            if(!s1.empty() && c == '#') {
-                s1.pop();
-                continue;
-            }
-            
-            if(s1.empty() && c == '#') continue;
-            
-            s1.push(c);
-        }
-        
-        for(char &c: t){
-            if(!s2.empty() && c == '#') {
-                s2.pop();
-                continue;
-            }
-            
-            if(s2.empty() && c == '#') continue;
-            
-            s2.push(c);
-        }
-        
-        if(s1.size() != s2.size()) return false;
-        
-        int stackSize = s1.size();
-        for(int i = 0; i < stackSize; i++){
+        stack<char> s1, s2;
+        s1 = removeBackspace(s);
+        s2 = removeBackspace(t);
+        while(!s1.empty() && !s2.empty()){
             if(s1.top() != s2.top()) return false;
             s1.pop();
             s2.pop();
         }
-        return true;
+        return s1.empty() == s2.empty();
     }
 };
