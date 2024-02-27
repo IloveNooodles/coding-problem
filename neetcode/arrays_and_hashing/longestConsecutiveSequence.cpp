b9ebfbde044b139a09246572a5e66e72673464a3
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2024-02-28 01:08:10 +0700

    Update longestConsecutiveSequence.cpp

--- neetcode/arrays_and_hashing/longestConsecutiveSequence.cpp
+++ neetcode/arrays_and_hashing/longestConsecutiveSequence.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    const int MAX_N = 1e5;
-    map<int, int> hash;
     int longestConsecutive(vector<int>& nums) {
-        
-        int maxCount = 0;
+        priority_queue<int, vector<int>, greater<int>> min_heap;
         int size = nums.size();
-
-        //insert
         for(int i = 0; i < size; i++){
-            hash[nums[i]] = i;
+            min_heap.push(nums[i]);
         }
-
+        int max_counter = 0;
+        int counter = 0;
+        int cur = 0;
         for(int i = 0; i < size; i++){
-            if(hash.find(nums[i] - 1) == hash.end()) //klo gaketemu berarti dia start
-            {
-                int start = nums[i] + 1; //itung dari i + 1 nya
-                while(hash.find(start) != hash.end()){
-                    start++;
-                }
-                maxCount = max(maxCount, start - nums[i]);
+            if(i == 0){
+                cur = min_heap.top();
+                min_heap.pop();
+                counter++;
+                continue;
             }
-        }
 
-        return maxCount;
+            int top = min_heap.top();
+            if(top - cur > 1) {
+                max_counter = max(max_counter, counter);
+                counter = 0;
+            }
+            if(top == cur) counter--;
+            cur = top;
+            min_heap.pop();
+            counter++;
+        }
+    
+        return max(max_counter, counter);
     }
 };
