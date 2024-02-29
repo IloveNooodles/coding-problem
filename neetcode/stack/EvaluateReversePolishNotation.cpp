cba1433b4f14f5aac8577c39e78f7dc34c385d02
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2024-03-01 02:09:49 +0700

    Update EvaluateReversePolishNotation.cpp

--- neetcode/stack/EvaluateReversePolishNotation.cpp
+++ neetcode/stack/EvaluateReversePolishNotation.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<string> number;
+        stack<string> s;
         int size = tokens.size();
         for(int i = 0; i < size; i++){
-            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
-                int op2 = std::stoi(number.top());
-                number.pop();
-                int op1 = std::stoi(number.top());
-                number.pop();
-                int res;
-                if(tokens[i] == "+"){
-                    res = op1 + op2; 
-                } else if (tokens[i] == "-") {
-                    res = op1 - op2;
-                } else if (tokens[i] == "*"){
-                    res = op1 * op2;
-                } else {
-                    res = op1 / op2;
-                }
-                number.push(to_string(res));
-            } else {
-                number.push(tokens[i]);
+            if(isValidOp(tokens[i])){
+                int num2 = stoi(s.top()); s.pop();
+                int num1 = stoi(s.top()); s.pop();
+                int result = calculate(num1, num2, tokens[i]);
+                s.push(to_string(result));
+                continue;
             }
+
+            s.push(tokens[i]);
         }
 
-        return std::stoi(number.top());
+        return stoi(s.top());
+    }
+
+    bool isValidOp(string input){
+        return input == "+" || input == "*" || input == "/" || input == "-";
+    }
+
+    int calculate(int num1, int num2, string op){
+        if(op == "+") return num1 + num2;
+        if(op == "*") return num1 * num2;
+        if(op == "-") return num1 - num2;
+        return num1 / num2;
     }
 };
