60ead1f2604bfe92699c7aaf5a7c430dd45c713f
IloveNooodles <mgarebaldhie80@gmail.com>
2025-03-12 09:32:52 +0900

    feat: ABC 395

---
+++ Atcoder/ABC 395/c.cpp
@@ -0,0 +1,88 @@
+#include "cmath"
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using lb = long double;
+using str = string;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pii = pair<int, pi>;
+using vi = vector<int>;
+using vii = vector<pi>;
+using vs = vector<string>;
+
+#define dbg(x)                                                                 \
+  cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ft front()
+#define bk back()
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)((x).size())
+#define fill(x, y) memset(y, x, sizeof(y))
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+
+const int MOD = 1e9 + 7;
+const ll BIG = 1e18;
+
+void setIO(string s) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  if (s != "") {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+  }
+}
+
+void solve() {
+  int n;
+  map<int, vi> m;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+    m[v[i]].pb(i);
+  }
+
+  int shortest = INT_MAX;
+  for (int i = 0; i < n; i++) {
+    int check = v[i];
+    if (m[check].size() > 1) {
+      int c = abs(m[check][0] - m[check][1]) + 1;
+      shortest = min(shortest, c);
+    }
+  }
+
+  if (shortest == INT_MAX)
+    cout << -1;
+  else
+    cout << shortest;
+}
+
+int main() {
+  setIO("");
+  solve();
+}
---
+++ Atcoder/ABC 395/d.cpp
@@ -0,0 +1,83 @@
+#include "cmath"
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using lb = long double;
+using str = string;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pii = pair<int, pi>;
+using vi = vector<int>;
+using vii = vector<pi>;
+using vs = vector<string>;
+
+#define dbg(x)                                                                 \
+  cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ft front()
+#define bk back()
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)((x).size())
+#define fill(x, y) memset(y, x, sizeof(y))
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+
+const int MOD = 1e9 + 7;
+const ll BIG = 1e18;
+
+void setIO(string s) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  if (s != "") {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+  }
+}
+
+void solve() {
+  int n, q;
+  map<int, vector<int>> nest(n, {1});
+  cin >> n >> q;
+  for (int i = 0; i < q; i++) {
+    int type;
+    cin >> type;
+    if (type == 1) {
+      int a, b;
+      cin >> a >> b;
+    } else if (type == 2) {
+      int a, b;
+      cin >> a >> b;
+    } else {
+      int a;
+      cin >> a;
+    }
+  }
+}
+
+int main() {
+  setIO("");
+  solve();
+}
