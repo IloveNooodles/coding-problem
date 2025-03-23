7aac6998128a5520a68836250baea5e71a3c9fd9
IloveNooodles <mgarebaldhie80@gmail.com>
2025-03-23 22:57:35 +0900

    feat: ABC 398

---
+++ Atcoder/ABC 398/b.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using lb = long double;
+using str = string;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pii = pair<int, pi>;
+using vi = vector<int>;
+using vii = vector<pi>;
+
+#define dbg(x)                                                                 \
+  cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ft front()
+#define bk back()
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)((x).size())
+#define fill(x, y) memset(y, x, sizeof(y))
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+
+const int MOD = 1e9 + 7;
+const ll BIG = 1e18;
+
+void setIO(string s) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  if (s != "") {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+  }
+}
+
+void solve() {
+  map<int, int> m;
+  vector<int> twoes, threes;
+  for (int i = 0; i < 7; i++) {
+    int x;
+    cin >> x;
+    m[x]++;
+
+    if (m[x] >= 3)
+      threes.pb(x);
+    if (m[x] >= 2)
+      twoes.pb(x);
+  }
+
+  for (int i = 0; i < threes.size(); i++) {
+    for (int j = 0; j < twoes.size(); j++) {
+      if (threes[i] != twoes[j]) {
+        cout << "Yes";
+        return;
+      }
+    }
+  }
+
+  cout << "No";
+}
+
+int main() {
+  setIO("");
+  solve();
+}
