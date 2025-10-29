61dcaa54f5cfc7957f2791ed862bbeb7d92428b5
IloveNooodles <mgarebaldhie80@gmail.com>
2025-10-29 10:30:19 +0900

    feat: codeforces 1062

---
+++ Codeforces/Codeforces Round 1062/b.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using lb = long double;
+using str = string;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pii = pair<int, pi>;
+using vi = vector<int>;
+using vii = vector<pi>;
+
+#define dbg(x)                                                                 \
+  cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ft front()
+#define bk back()
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)((x).size())
+#define fill(x, y) memset(y, x, sizeof(y))
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+
+const int MOD = 1e9 + 7;
+const ll BIG = 1e18;
+
+void setIO(string s) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  if (s != "") {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+  }
+}
+
+void solve() {
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    int arr[26] = {0};
+    int size;
+    cin >> size;
+    string s, t;
+    cin >> s >> t;
+    for (int j = 0; j < size; j++) {
+      arr[s[j] - 'a']++;
+    }
+
+    for (int j = 0; j < size; j++) {
+      arr[t[j] - 'a']--;
+    }
+
+    bool valid = true;
+    for (int i = 0; i < 26; i++) {
+      if (arr[i] != 0) {
+        valid = false;
+        break;
+      }
+    }
+
+    if (valid) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }
+}
+
+int main() {
+  setIO("");
+  solve();
+}
