10b29969e377b9d5edb3f828a571c4ad94b442f5
IloveNooodles <mgarebaldhie80@gmail.com>
2026-01-08 23:14:42 +0900

    feat: add censoring

---
+++ USACO/simulation/censoring.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using lb = long double;
+using str = string;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pii = pair<int, pi>;
+using vi = vector<int>;
+using vii = vector<pi>;
+
+#define dbg(x)                                                                 \
+  cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ft front()
+#define bk back()
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)((x).size())
+#define fill(x, y) memset(y, x, sizeof(y))
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+
+const int MOD = 1e9 + 7;
+const ll BIG = 1e18;
+void setIO(string s) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  if (s != "") {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+  }
+}
+
+void solve() {
+  string N;
+  cin >> N;
+  string T;
+  cin >> T;
+
+  string ans = "";
+
+  int nz = N.size();
+  int tz = T.size();
+
+  int ptr = 0;
+  for (int i = 0; i < nz; i++) {
+    int az = ans.size();
+    int right = az - 1;
+    int left = right - tz + 1;
+    ans += N[i];
+    if (az >= tz) {
+      if (ans.substr(left, tz) == T) {
+        ans.erase(az - tz, tz);
+      }
+    }
+  }
+
+  cout << ans << endl;
+}
+
+int main() {
+  setIO("censor");
+  solve();
+}
