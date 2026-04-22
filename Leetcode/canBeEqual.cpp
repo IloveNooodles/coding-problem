724fd0c9392357d0a0b69a5c915f9e57278ccb62
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2026-04-23 00:06:22 +0900

    Create canBeEqual.cpp

---
+++ Leetcode/canBeEqual.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+       if(s1 == s2) return true;
+       else {
+        for(int i = 0; i < 2; i++){
+            if(s1[i] == s2[i]) continue;
+            else {
+                char temp = s1[i + 2];
+                s1[i + 2] = s1[i];
+                s1[i] = temp;
+            };
+        }
+
+        for(int i = 0; i < 4; i++){
+            if(s1[i] != s2[i]) return false;
+        }
+       } 
+
+       return true;
+    }
+};
