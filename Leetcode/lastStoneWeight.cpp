dfaafa99a42ee50106ddd01fdd9c0597c38ffc66
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2026-05-15 03:14:13 +0900

    Implement lastStoneWeight function using priority queue

---
+++ Leetcode/lastStoneWeight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i = 0; i < stones.size(); i++){
+            pq.push(stones[i]);
+        }
+
+        while(pq.size() > 1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+
+            y = y - x;
+            if(y == 0) continue;
+            else pq.push(y);
+        }
+
+        return pq.empty() ? 0 : pq.top();
+    }
+};
