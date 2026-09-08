2aade140769fb641f1ce28a18af31b8ad6be1b90
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2026-09-09 00:45:37 +0900

    Add function to count smaller numbers than current
    Implement a function to count smaller numbers for each element in the input array.

---
+++ Leetcode/smallerNumbersThanCurrent.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            int cur = nums[i], cnt = 0;
+            for(int j = 0; j < nums.size(); j++){
+                if(i == j) continue;
+                if(nums[i] > nums[j]) cnt++;
+            }
+
+            ans.push_back(cnt);
+        }
+
+        return ans;
+    }
+};
